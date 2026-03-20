2823e83458d0af1ebb84bbe33adc61d8aa0f5c36
Rudraksh sachdev <rudrakshsachdeva04@gmail.com>
2026-03-20 18:41:05 +0530

    Learning Backend operations using FastAPI

--- fastAPI-Ecommerce/app/main.py
+++ fastAPI-Ecommerce/app/main.py
@@ -7,3 +7,13 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"message": "Welcome to the FastAPI Ecommerce API"}
+
+# define a route for the users URL
+@app.get("/users")
+def get_users():
+    return ["Yug Prakash", "Rudraksh Sachdeva", "Aarav", "Shivansee"]
+
+# define a route for the users URL with a path parameter
+@app.get("/users/{user_id}")
+def get_user(user_id: int):
+    return {"user_id": user_id}
