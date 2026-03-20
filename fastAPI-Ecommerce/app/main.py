e154a5f45e9444de2d4ed7bedbc658c1a395a485
Rudraksh sachdev <rudrakshsachdeva04@gmail.com>
2026-03-20 19:17:27 +0530

    feat: Learning Backend Operations using FastAPI

--- fastAPI-Ecommerce/app/main.py
+++ fastAPI-Ecommerce/app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Query
 
 from services.products import get_all_products
 
@@ -26,6 +26,37 @@ def get_user(user_id: int):
     return {"user_id": user_id}
 
 # define a route for the products URL
-@app.get("/products")
+@app.get("/product")
 def get_products():
     return get_all_products()
+
+
+@app.get("/products")
+# define a query parameter for the products URL with a default value of None and a minimum length of 1 and a maximum length of 50 and a description of "Search by product name (case insensitive)"
+def list_products(
+    name : str = Query(
+        default=None, 
+        min_length=1, 
+        max_length=50, 
+        description="Search by product name (case insensitive)")
+    ):
+
+    # list out all the products
+    products = get_all_products()
+
+    # if the name is not None, then filter the products based on the name
+    if name:
+        # strip the name of any leading or trailing whitespace and convert it to lowercase
+        needle = name.strip().lower()
+        # filter the products based on the name
+        products = [p for p in products if needle in p.get("name", "").lower()]
+
+        # if the products are not found, then raise an HTTPException
+        if not products:
+            raise HTTPException(status_code=404, detail="Product not found")
+        
+        # get the total number of products
+        total = len(products)
+    
+    # return the total number of products and the list of products
+    return {"total": total, "products": products}
